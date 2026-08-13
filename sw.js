edd6b340dd0dfb0bddc0b8d3e502b4a6a394e4e2
JAGESH ELECTRONIC <jageshelectronic@gmail.com>
2026-08-13 09:11:48 +0530

    Add Supabase configuration placeholders
    Added Supabase URL and key placeholders for configuration.

--- sw.js
+++ sw.js
@@ -1,1 +1,2 @@
 const CACHE="jagesh-v14-final";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const SUPABASE_URL="REPLACE_WITH_YOUR_SUPABASE_URL"SUPABASE_KEY="REPLACE_WITH_YOUR_PUBLISHABLE_KEY"
